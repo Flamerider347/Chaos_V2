bf1b880e6429369bc01c98a06c3b5e5152294cf3
Flamerider347 <flamerider995@gmail.com>
2026-05-04 12:32:20 +1200

    Mathew plays russian godot roulette

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,7 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const GRAVITY = 9.8
 const MOUSE_SENSITIVITY = 0.003
+@onready var interact_cast = $head/interact_cast
 
 func _ready() -> void:
 	GDSync.connect_gdsync_owner_changed(self, owner_changed)
@@ -34,6 +35,10 @@ func _physics_process(delta: float) -> void:
 		velocity.y -= GRAVITY * delta
 	
 	if not GameData.paused:
+		if Input.is_action_just_pressed("left_click"):
+			if interact_cast.is_colliding():
+				if interact_cast.get_collider().has_method():
+					print("Hi")
 		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
 	
