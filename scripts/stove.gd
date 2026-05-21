e8b5e880a780d25bbbdd843dadd9bd518c940742
Flamerider347 <flamerider995@gmail.com>
2026-05-21 13:19:11 +1200

    stovestuff

---
+++ scripts/stove.gd
@@ -0,0 +1,20 @@
+extends StaticBody3D
+const COOKED_SCENES = {
+	"cheese": preload("res://Prefabs/cheese_chopped.tscn"),
+	"tomato": preload("res://Prefabs/tomato_chopped.tscn")
+}
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("choppable"):
+		chop(body)
+
+func chop(body: Node3D) -> void:
+	body.queue_free()
+	if not GDSync.is_host():
+		return
+	if COOKED_SCENES.has(body.type):
+		var chopped = GDSync.multiplayer_instantiate(COOKED_SCENES[body.type], get_node("/root/main/game/items"), true, [], true)
+		chopped.global_position = global_position + Vector3(0, 0.5, 0)
+		chopped.add_to_group("plate_stackable")
+		chopped.type = body.type + "_chopped"
